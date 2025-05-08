ecad570ae92b0c1b8339ca9352fa904f962712f8
abidm-bit <abidmres@gmail.com>
2025-05-08 12:50:26 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("kevin, what do u mean see the world or sea world?");
+    printf("\n");
+    printf("see world: oceans, fish, CHINA ");
+    return 0;
+}
