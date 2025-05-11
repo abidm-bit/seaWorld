9d6fde9d336902c1e9b0c069345698e67abeb720
abidm-bit <abidmres@gmail.com>
2025-05-10 20:18:15 -0400

    an explicit function to add 2 numbers

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+/*  header file:  #include <stdio.h>
+ *  int main(void){
+ *  return 0
+ *  }
+ *
+ *   In C, main is the entry point, like psvm / void main() in Java
+ *   return 0 = indicates success
+ *   non zero = failure
+ *
+ *
+ */
+
+int sum2digits(int a, int b) {
+    return a+b;
+}
+
 int main(void) {
     printf("kevin, what do u mean see the world or sea world?");
     printf("\n");
     printf("see world: oceans, fish, CHINA ");
+    printf("\n");
+    int add = sum2digits(11, 15);
+    printf("Sum is: %d", add);
     return 0;
 }
